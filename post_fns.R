5e3a8de551c63b112e5d59aba624503eabac14f6
chittaranjan19 <vrchittaranjan@gmail.com>
2022-12-05 17:31:03 -0500

    fix typos

--- post_fns.R
+++ post_fns.R
@@ -1,6 +1,6 @@
 sample_post_beta = function(theta, T_mat, y_g, X_g, sigma2_g) {
-    new_cov = solve(solve(T_mat) + t(X)%*%X/sigma2_g)
-    new_mean = new_cov%*%(solve(T_mat)%*%theta + t(X)%*%y/sigma2_g)
+    new_cov = solve(solve(T_mat) + t(X_g)%*%X_g/sigma2_g)
+    new_mean = new_cov%*%(solve(T_mat)%*%theta + t(X_g)%*%y_g/sigma2_g)
     new_beta = rmvnorm(1, mean = new_mean, sigma = new_cov)
     return(new_beta)
 }
@@ -21,8 +21,8 @@ sample_post_v = function(old_v) {
 }
 
 sample_post_theta = function(mu_0, w_0, beta_vec, X_g, sigma2_g) {
-    new_cov = solve(solve(w_0) + t(X)%*%X/sigma2_g)
-    new_mean = new_cov%*%(solve(w_0)%*%mu_0 + t(X)%*%beta_vec/sigma2_g)
+    new_cov = solve(solve(w_0) + t(X_g)%*%X_g/sigma2_g)
+    new_mean = new_cov%*%(solve(w_0)%*%mu_0 + t(X_g)%*%beta_vec/sigma2_g)
     new_theta = rmvnorm(1, mean = new_mean, sigma = new_cov)
     return(new_theta)
 }
